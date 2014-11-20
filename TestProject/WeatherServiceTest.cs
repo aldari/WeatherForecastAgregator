d61c3a34fb7967a023da0bf802a5e5d17d3628f8
El'dar Ismagilov <eldarismagilov@gmail.com>
2014-11-20 11:36:32 +0500

    Test Project

---
+++ TestProject/WeatherServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using NUnit.Framework;
+
+namespace TestProject
+{
+    [TestFixture]
+    public class WeatherServiceTest
+    {
+        [Test]
+        public void FirstTest()
+        {
+            const string magicString = "http://api.wunderground.com/api/7b9175d0dab642ae/forecast10day/lang:RU/conditions/q/Russia/{0}.xml";
+            var req = WebRequest.Create(String.Format(magicString, "Chelyabinsk"));
+            WebResponse resp = req.GetResponse();
+            Stream stream = resp.GetResponseStream();
+            if (stream != null)
+            {
+                var sr = new StreamReader(stream);
+                Console.WriteLine(sr.ReadToEnd());
+                sr.Close();
+            }
+        }
+    }
+}
