ffd96517e3c312a0f29354c5cb77f913edbf69fc
El'dar Ismagilov <eldarismagilov@gmail.com>
2014-11-23 19:21:08 +0500

    Added EnglishToRussianConvertor, missing in prevous commit

---
+++ Services/EnglishToRussianConvertor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+namespace Services
+{
+    public static class EnglishToRussianConvertor
+    {
+        public static String ToRussianWind(this String input)
+        {
+            var sb = new StringBuilder();
+            foreach (var ch in input)
+            {
+                char newch = '\0';
+                switch (ch)
+                {
+                    case 'N':
+                        newch = 'С';
+                        break;
+                    case 'E':
+                        newch = 'В';
+                        break;
+                    case 'S':
+                        newch = 'Ю';
+                        break;
+                    case 'W':
+                        newch = 'З';
+                        break;
+                }
+                sb.Append(newch);
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ TestProject/EnglishToRussianConvertorTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using Services;
+
+namespace TestProject
+{
+    [TestFixture]
+    public class EnglishToRussianConvertorTest
+    {
+        [Test]
+        public void WindDirectionConvertionTest()
+        {
+            const string input = "NNW";
+
+            var result = input.ToRussianWind();
+
+            Assert.AreEqual("ССЗ", result);
+        }
+    }
+}
