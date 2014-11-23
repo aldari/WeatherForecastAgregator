efc1e90aba7eeb257032e718a5ff5adbc79dc2b2
El'dar Ismagilov <eldarismagilov@gmail.com>
2014-11-23 11:53:41 +0500

    Wunderground.ParseNode changed to private Removed wundergroundNode.txt

--- Services/WundergroundForecast.cs
+++ Services/WundergroundForecast.cs
@@ -1,28 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Xml.Linq;
 
 namespace Services
 {
     public class WundergroundForecast
     {
-        public WundergroundForecast()
-        {
-        }
-
         public IEnumerable<ForecastDto> ForecastData(String str)
         {
             XElement xelement = XElement.Parse(str);
             var forecastNodes = xelement.Element("forecast").Element("simpleforecast").Element("forecastdays").Elements();
-            var list = new List<ForecastDto>();
-            foreach (var forecastNode in forecastNodes)
-            {
-                list.Add(new ForecastDto());
-            }
-            return list;
+            return forecastNodes.Select(ParseNode).ToList();
         }
 
-        public ForecastDto ParseNode(XElement node)
+        private ForecastDto ParseNode(XElement node)
         {
             var date = new DateTime(
                 int.Parse(node.Element("date").Element("year").Value),
--- TestProject/WeatherServiceTest.cs
+++ TestProject/WeatherServiceTest.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
-using System.Xml.Linq;
-using Moq;
 using NUnit.Framework;
 using Services;
 
@@ -11,7 +9,6 @@ namespace TestProject
     [TestFixture]
     public class WeatherServiceTest
     {
-        [Test]
         public void FirstTest()
         {
             const string magicString = "http://api.wunderground.com/api/7b9175d0dab642ae/forecast10day/lang:RU/conditions/q/Russia/{0}.xml";
@@ -24,39 +21,12 @@ namespace TestProject
         }
 
         [Test]
-        public void ThirdTest()
-        {
-            String str = File.ReadAllText(@"mock/wundergroundNode.txt");
-            var node = XElement.Parse(str);
-            
-            var date = new DateTime(
-                int.Parse(node.Element("date").Element("year").Value),
-                int.Parse(node.Element("date").Element("month").Value),
-                int.Parse(node.Element("date").Element("day").Value)
-            );
-            var description = node.Element("conditions").Value;
-            var humidity = int.Parse(node.Element("avehumidity").Value);
-            var windSpeed = int.Parse(node.Element("avewind").Element("kph").Value);
-            var windDirection = node.Element("avewind").Element("dir").Value;
-            var minTemperature = int.Parse(node.Element("low").Element("celsius").Value);
-            var maxTemperature = int.Parse(node.Element("high").Element("celsius").Value);
-
-            Assert.AreEqual(new DateTime(2014, 11, 21), date);
-            Assert.AreEqual("Облачно", description);
-            Assert.AreEqual(81, humidity);
-            Assert.AreEqual(18, windSpeed);
-            Assert.AreEqual("ЗСЗ", windDirection);
-            Assert.AreEqual(-6, minTemperature);
-            Assert.AreEqual(1, maxTemperature);
-        }
-
-        [Test]
         public void WundergroundForecastTest()
         {
-            String str = File.ReadAllText(@"mock/wundergroundAll.txt");
+            String content = File.ReadAllText(@"mock/wundergroundAll.txt");
             var wundergroundService = new WundergroundForecast();
 
-            var result = wundergroundService.ForecastData(str);
+            var result = wundergroundService.ForecastData(content);
 
             Assert.IsNotNull(result);
             Assert.AreEqual(10, result.Count());
@@ -65,11 +35,11 @@ namespace TestProject
         [Test]
         public void ParseXmlForWundergroundForecast()
         {
-            String str = File.ReadAllText(@"mock/wundergroundNode.txt");
+            String content = File.ReadAllText(@"mock/wundergroundAll.txt");
             var wundergroundService = new WundergroundForecast();
 
-            var node = XElement.Parse(str);
-            var dto = wundergroundService.ParseNode(node);
+            var result = wundergroundService.ForecastData(content);
+            var dto = result.First();
 
             Assert.AreEqual(new DateTime(2014, 11, 21), dto.Date);
             Assert.AreEqual("Облачно", dto.Description);
