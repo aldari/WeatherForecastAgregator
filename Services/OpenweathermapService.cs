0e4082ca3f5d113fc11edfa288cd75053a4d5b34
El'dar Ismagilov <eldarismagilov@gmail.com>
2014-11-23 17:39:25 +0500

    OpenweathermapService added

---
+++ Services/OpenweathermapService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace Services
+{
+    public class OpenweathermapService
+    {
+
+        public IEnumerable<ForecastDto> ForecastData(string content)
+        {
+            var xelement = XElement.Parse(content);
+            return xelement.Element("forecast").Elements().Select(ParseNode).ToList();
+        }
+
+        private ForecastDto ParseNode(XElement node)
+        {
+            return new ForecastDto
+            {
+                Date = DateTime.Parse(node.Attribute("day").Value, CultureInfo.CurrentCulture),
+                Description = node.Element("symbol").Attribute("name").Value,
+                Humidity = Int32.Parse(node.Element("humidity").Attribute("value").Value),
+                MaxTemperature = (int)Single.Parse(node.Element("temperature").Attribute("max").Value, CultureInfo.InvariantCulture),
+                MinTemperature = (int)Single.Parse(node.Element("temperature").Attribute("min").Value, CultureInfo.InvariantCulture),
+                WindDirection = node.Element("windDirection").Attribute("code").Value.ToRussianWind(),
+                WindSpeed = (Int32)Math.Round(Single.Parse(node.Element("windSpeed").Attribute("mps").Value, CultureInfo.InvariantCulture))
+            };
+        }
+    }
+}
---
+++ TestProject/OpenweathermapServiceTest.cs
@@ -0,0 +1,52 @@
+﻿using System.Globalization;
+using System.Xml.Linq;
+using NUnit.Framework;
+using Services;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace TestProject
+{
+    public class OpenweathermapServiceTest 
+    {
+        public void Test()
+        {
+            const string magicString = "http://api.openweathermap.org/data/2.5/forecast/daily?q={0}&mode=xml&units=metric&cnt=10&lang=ru";
+            var uri = String.Format(magicString, "Chelyabinsk");
+
+            var xmld = new QueryLoader().LoadData(uri);
+            File.WriteAllText("openweathermap.txt", xmld);
+        }
+
+        [Test]
+        public void OpenweathermapServiceReturnForecastsTest()
+        {
+            String content = File.ReadAllText(@"mock/openweathermap.txt");
+
+            var service = new OpenweathermapService();
+            var result = service.ForecastData(content);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(10, result.Count());
+        }
+
+        [Test]
+        public void XmlNodeParseTest()
+        {
+            String content = File.ReadAllText(@"mock/openweathermap.txt");
+            var service = new OpenweathermapService();
+
+            var result = service.ForecastData(content);
+            var dto = result.First();
+            
+            Assert.AreEqual(new DateTime(2014, 11, 23), dto.Date);
+            Assert.AreEqual("небольшой снегопад", dto.Description);
+            Assert.AreEqual(92, dto.Humidity);
+            Assert.AreEqual(-13, dto.MaxTemperature);
+            Assert.AreEqual(-22, dto.MinTemperature);
+            Assert.AreEqual("ССВ", dto.WindDirection);
+            Assert.AreEqual(7, dto.WindSpeed);
+        }
+    }
+}
